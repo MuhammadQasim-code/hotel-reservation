e000aa22155537f62c03f3871d051cdc23d9299d
firstrepo1 <shaikhqasim166@gmail.com>
2026-07-10 16:09:49 +0500

    fix: append .id to route table associations in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,22 +142,22 @@ resource "aws_route_table" "private_rt" {
 # Route Table Associations
 resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public_rt
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public_rt
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "private_app_1" {
   subnet_id      = aws_subnet.private_app_1.id
-  route_table_id = aws_route_table.private_rt
+  route_table_id = aws_route_table.private_rt.id
 }
 
 resource "aws_route_table_association" "private_app_2" {
   subnet_id      = aws_subnet.private_app_2.id
-  route_table_id = aws_route_table.private_rt
+  route_table_id = aws_route_table.private_rt.id
 }
 
 # --------------------------------------------------------
